ae7d7280bff383f56400e755eb783cc274e81228
Mary McCallum <mary.mccallum@os.uk>
2023-08-07 22:56:38 +0100

    Challenges completed

--- index.js
+++ index.js
@@ -16,9 +16,3 @@ app.listen(PORT, () =>
 app.get("/", (req, res) => {
   return res.send("Exercise Tracker");
 });
-
-// Retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
-app.get("/api/exercise/log", (req, res) => {});
-
-// Retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
-app.get("/");
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,21 @@
 import express from "express";
-import bodyParser from "body-parser";
-
 const router = express.Router();
 
 let users = [];
 
+// Get an array of all users by getting api/exercise/users
+// with the same info as when creating a user.
 router.get("/users", (req, res) => {
   res.send(users);
+});
+
+//Get an individual record
+router.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+
+  const foundUser = users.find((user) => user.id == id);
+
+  res.send(foundUser);
 });
 
 // Create a user by posting form data username to /api/exercise/new-user and returned will be
@@ -22,22 +31,6 @@ router.post("/new-user", (req, res) => {
   res.send(
     `New user object: { username: ${newUser.username}, id: ${newUser.id} }`
   );
-});
-
-// Get an array of all users by getting api/exercise/users
-// with the same info as when creating a user.
-router.get("/api/exercise/users", (req, res) => {
-  res.send(users);
-});
-
-//Get an individual record
-
-router.get("/users/:id", (req, res) => {
-  let id = req.params.id;
-
-  const foundUser = users.find((user) => user.id == id);
-
-  res.send(foundUser);
 });
 
 //Delete a user
@@ -57,6 +50,7 @@ Returned will be the user object with also with the exercise fields added.*/
 router.patch("/add", (req, res) => {
   const id = req.body.id;
   const exercise = req.body.exercise;
+  const exercise2 = req.body.exercise2;
   const duration = req.body.duration;
   const date = req.body.date;
 
@@ -76,6 +70,9 @@ router.patch("/add", (req, res) => {
   if (req.body.exercise) {
     user.exercise = exercise;
   }
+  if (req.body.exercise2) {
+    user.exercise = exercise2;
+  }
   if (req.body.duration) {
     user.duration = duration;
   }
@@ -84,6 +81,37 @@ router.patch("/add", (req, res) => {
   }
 
   res.send(user);
+});
+
+// Retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id).
+// Return will be the user object with added array log and count (total exercise count).
+router.get("/log/:id", (req, res) => {
+  let id = req.params.id;
+  const chosenUser = users.find((user) => user.id == id); // filter function to find the user we want to update
+
+  // let id = req.params.id;
+
+  // const foundUser = users.find((user) => user.id == id);
+
+  console.log(chosenUser);
+
+  let log = [];
+  let exercise = chosenUser.exercise;
+  let exercise2 = chosenUser.exercise2;
+
+  if (exercise && exercise2) {
+    log.push(exercise);
+    log.push(exercise2);
+  } else if (exercise) {
+    log.push(exercise);
+  } else {
+    log.push(exercise2);
+  }
+
+  const count = log.length;
+
+  const userArray = { ...chosenUser, log: log, count: count };
+  res.send(userArray);
 });
 
 export default router;
